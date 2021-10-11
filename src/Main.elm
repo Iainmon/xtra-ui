ca00d27610b4e314ba5acc00704459f6c86e9e66
Jack Barnes <barnesjt05@gmail.com>
2021-10-11 03:05:11 -0700

    working svg display

--- src/Main.elm
+++ src/Main.elm
@@ -10,12 +10,14 @@ import Http
 import Json.Decode as D
 import Json.Encode as JE
 import Draggable exposing (init)
+import SvgParser exposing(parse)
+import String exposing(split)
 
-type alias Model = { program : String, filter : String, image : String}
+type alias Model = { program : String, filter : String, dotString : String, image : String}
 
 init : () -> (Model, Cmd Msg)
 init _ = 
-    ( { program = "", filter = "", image = ""}
+    ( { program = "", filter = "", dotString = "", image = ""}
     , Cmd.none
     )
 
@@ -34,13 +36,19 @@ xtraBackendURL = "http://localhost:8081/trace"
 view : Model -> Html Msg
 view model =
     div []
-        [textarea [rows 8, cols 60] [text <| .program model]
+        [textarea [rows 8, cols 60, onInput SaveProg] [text <| .program model]
         ,hr [] []
-        ,textarea [rows 8, cols 60] [text <| .filter model]
+        ,textarea [rows 8, cols 60, onInput SaveFilt] [text <| .filter model]
         ,hr [] []
         ,button [onClick Clear] [text "Clear"]
         ,button [onClick Run] [text "Run"]
         ,button [onClick LoadEx] [text "LoadEx"]
+        ,hr [] []
+        ,div [] [text <| .dotString model]
+        ,hr [] []
+        ,svgOrNot model
+        ,hr [] []
+        ,div [] [text <| cleanSvgString <| .image model]
         ,hr [] []
         ,div [] [text <| .image model]
         ]
@@ -48,8 +56,30 @@ view model =
 type Msg 
     = Run
     | Clear
-    | LoadEx
+    | LoadEx 
+    | SaveProg String
+    | SaveFilt String
     | GotDot (Result Http.Error String)
+    | GotSvg (Result Http.Error String)
+
+svgOrNot : Model -> Html Msg
+svgOrNot model = 
+    if .image model == "" then
+        div [] []
+    else
+        case parse (cleanSvgString (.image model)) of
+            Err msg -> div [] [text msg]
+            Ok elem -> elem
+
+cleanSvgString : String -> String
+cleanSvgString str 
+    = String.dropLeft 222 str
+    {--let res = List.head ( List.drop 1 (String.split "<!-- Pages: 1 --> " str)) in
+        case res of
+            Just svg -> svg
+            Nothing -> "Parsing Error"
+           --}
+
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
@@ -63,11 +93,25 @@ update msg model =
             (   {model | program = exampleProg, filter = exampleFilter, image = ""}
             , Cmd.none
             )
+        SaveProg prog ->
+            ( {model | program = prog}
+            , Cmd.none 
+            )
+        SaveFilt filt ->
+            ( {model | filter = filt}
+            , Cmd.none
+            )
         Run ->
             ( model, getDotString model )
         GotDot (Ok dot) ->
-            ( {model | image = dot}, Cmd.none)
+            ( {model | dotString = dot}, getSvg {model | dotString = dot} )
         GotDot (Err httpError) ->
+            ( { model | dotString = buildErrorMessage httpError}
+            , Cmd.none
+            )
+        GotSvg (Ok svg) ->
+            ( {model | image = svg}, Cmd.none)
+        GotSvg (Err httpError) ->
             ( { model | image = buildErrorMessage httpError}
             , Cmd.none
             )
@@ -76,15 +120,15 @@ update msg model =
 
 getDotString : Model -> Cmd Msg
 getDotString model = 
-{--
+{--}
     Http.post
-    {   url = xtraBackendURL
+    { url = xtraBackendURL
         , expect = Http.expectJson GotDot (D.field "dot" D.string)
         , body = Http.jsonBody <| JE.object [ ("prog", JE.string (.program model)), ("filter", JE.string (.filter model))]
     }
 --}
-{--}
-    Http.riskyRequest
+{--
+    Http.request
     {   url = xtraBackendURL
         , method = "GET"
         , headers = []
@@ -95,6 +139,13 @@ getDotString model =
     }
 --}
 
+getSvg : Model -> Cmd Msg
+getSvg model =
+    Http.post
+    { url = krokiURL
+        , expect = Http.expectString GotSvg
+        , body = Http.jsonBody <| JE.object [ ("diagram_source", JE.string (.dotString model)) ]
+    }
 
 buildErrorMessage : Http.Error -> String
 buildErrorMessage httpError =
